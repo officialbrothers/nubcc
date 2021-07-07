6806f43806c9a32d8c2878f8085abf2eae5959f0
officialbrothers <75666427+officialbrothers@users.noreply.github.com>
2021-07-07 14:01:48 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function parallax (element, distance, speed){
+    const item = document.querySelector(element);
+
+    item.style.transform = `translateY(${distance * speed}px)`;
+}
+
+window.addEventListener("scroll", function(){
+    parallax("header", window.scrollY, 1);
+    parallax(".drone", window.scrollY, 0.3);
+    parallax(".drone2", window.scrollY, 0.5);
+    parallax(".intro-text1", window.scrollY, 0.4);
+    parallax(".intro-text2", window.scrollY, 0.5);
+})
+
+
+
+gsap.form('.logo1', {opacity:0, duration:3, delay:.5, y:30, ease:'expo.out'})
+
